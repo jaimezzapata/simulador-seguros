107723d78af21debd8971aed0a08e427d2f2bd5f
jaimezzapata <zapataval2304@gmail.com>
2024-12-09 09:06:17 -0500

    Implementación de estilos al login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { useState } from "react";
+import "./Login.css";
 
 const Login = () => {
   const [getCorreo, setCorreo] = useState("");
@@ -6,22 +8,33 @@ const Login = () => {
   const [getUsuarios, setUsuarios] = useState([]);
 
   function iniciarSesion() {
-    // console.log(getContrasena, getCorreo);
     fetch("http://localhost:7714/usuarios")
       .then((response) => response.json())
       .then((data) => setUsuarios(data));
-  }
 
+    let auth = getUsuarios.some(
+      (item) => getCorreo === item.correo && getContrasena === item.contraseña
+    );
+    console.log(auth);
+  }
 
   return (
     <form>
       <div>
         <label htmlFor="">Correo</label>
-        <input onChange={(e) => setCorreo(e.target.value)} type="text" />
+        <input
+          onChange={(e) => setCorreo(e.target.value)}
+          type="text"
+          placeholder="Ingresa tu correo"
+        />
       </div>
       <div>
         <label htmlFor="">Contraseña</label>
-        <input onChange={(e) => setContrasena(e.target.value)} type="text" />
+        <input
+          onChange={(e) => setContrasena(e.target.value)}
+          type="password"
+          placeholder="Ingresa tu contraseña"
+        />
       </div>
       <button onClick={iniciarSesion} type="button">
         Iniciar Sesión
