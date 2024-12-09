1bdb89e9b943d989d7f4be8c08afa0b1fc977668
jaimezzapata <zapataval2304@gmail.com>
2024-12-09 10:36:50 -0500

    Implementación de sweetaler2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import { useState } from "react";
-import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
+import "./Login.css";
 const Login = () => {
   const [getCorreo, setCorreo] = useState("");
   const [getContrasena, setContrasena] = useState("");
   const [getUsuarios, setUsuarios] = useState([]);
+  let redireccion = useNavigate();
 
   function iniciarSesion() {
     fetch("http://localhost:7714/usuarios")
       .then((response) => response.json())
       .then((data) => setUsuarios(data));
-
     let auth = getUsuarios.some(
       (getUsuario) =>
         getUsuario.contraseña == getContrasena && getUsuario.correo == getCorreo
     );
-    console.log(auth);
+    if (auth) {
+      let timerInterval;
+      Swal.fire({
+        title: "Auto close alert!",
+        html: "I will close in <b></b> milliseconds.",
+        timer: 2000,
+        timerProgressBar: true,
+        icon: "success",
+        didOpen: () => {
+          Swal.showLoading();
+          const timer = Swal.getPopup().querySelector("b");
+          timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+          redireccion("/");
+        },
+      });
+    }
+    /* Validar inicio de sesión con usuario o correo */
   }
 
   return (
